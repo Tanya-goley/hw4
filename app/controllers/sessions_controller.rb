8a683a47f12a3fc3995d42c13e54c9779a1c40c4
Tanya Goley <tanya.goley@kellogg.northwestern.edu>
2025-03-17 18:54:45 +0000

    Added Signup, Login, Logout and current user display

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email])  # Find user by email
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id  # Log in user
+      redirect_to places_path, notice: "You are logged in successfully!"
+    else
+      flash[:alert] = "Invalid email or password!"
+      render :new
+    end
   end
 
   def destroy
+    session[:user_id] = nil  # Log out user
+    redirect_to login_path, notice: "You are logged out successfully!"
   end
 end
-  
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
   resources "places"
   resources "sessions"
   resources "users"
+  get "/signup", to: "users#new" #for the signup page
+  get "/login", to: "sessions#new" #for the login page
+  post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy" #for the logout option
 end
