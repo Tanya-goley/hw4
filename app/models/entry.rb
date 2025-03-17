f92229079b090c42e4b41e2f8d801fb1f776b7f2
Tanya Goley <tanya.goley@kellogg.northwestern.edu>
2025-03-17 21:48:48 +0000

    Add file validation

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,16 @@
 class Entry < ApplicationRecord
   belongs_to :place
   has_one_attached :image
+
+  validate :image_format
+
+  private
+
+  def image_format
+    return unless image.attached?
+
+    if !image.content_type.in?(%w[image/jpeg image/png image/gif])
+      errors.add(:image, "must be a JPG, PNG, or GIF file")
+    end
+  end
 end
